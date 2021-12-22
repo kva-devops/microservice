5b3d3b0f88ecad02e3796f2269999f4cd5b837ea
vladimirKutyavin <kva.devops@gmail.com>
2021-12-22 15:12:49 +0400

    create microservice

---
+++ src/test/java/ru/job4j/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
